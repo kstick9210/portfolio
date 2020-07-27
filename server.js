714d88141e77eb7f9d255abfdb82917318a33417
Kathleen Stickel <kstick@Kathleens-MBP.lan>
2020-07-27 17:59:13 -0500

    add server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+const port = process.env.PORT || 3001;
