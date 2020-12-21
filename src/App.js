2afaf43328b26d03df01b75f6abe3fa429644704
Kathleen Stickel <kstick9210@outlook.com>
2020-12-21 18:55:11 -0500

    style links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import ScrollDownArrow from './components/ScrollDownArrow/ScrollDownArrow';
+import Links from "./components/Links/Links";
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Skills from './components/Skills/Skills';
@@ -10,10 +10,7 @@ function App() {
   return (
     <main>
       <Header />
-      <ScrollDownArrow />
-      <About />
-      <Projects />
-      <Skills />
+      <Links />
     </main>
   );
 }
---
+++ src/components/Links/Links.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Links.css";
+
+export default function Links() {
+    return (
+        <div className="Links">
+            <a href="https://www.linkedin.com/in/kathleen-stickel/" target="_blank" rel="noopener noreferrer">
+                    <img src="https://i.imgur.com/5YPzVDK.png" className="social-icon" id="li" alt="LinkedIn Logo"></img>
+            </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <div className="triangles">
+                <div className="up"><p>About<br />Me</p></div>
+                <div className="down"><p>My Projects</p></div>
+            </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="https://github.com/kstick9210" target="_blank" rel="noopener noreferrer">
+                    <img src="https://i.imgur.com/3J01ITc.png" className="social-icon" alt="GitHub Logo"></img>
+            </a>
+        </div>
+    )
+}
