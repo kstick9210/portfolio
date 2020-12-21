b2c65467bcf14b7695069955e7150b109f8792c8
Kathleen Stickel <kstick9210@outlook.com>
2020-12-21 17:46:19 -0500

    add header image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../assets/images/portfolio_logo.png';
 
 export default function Header() {
     return (
         <div className="Header">
-            
+            <img src={logo} alt="Kathleen Stickel, Software Engineer"></img>
         </div>
     )
 }
