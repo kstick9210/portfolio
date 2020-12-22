8f009834e31bfc269ce147c467f7b104212e7573
Kathleen Stickel <kstick9210@outlook.com>
2020-12-21 19:00:23 -0500

    update social icons

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -5,14 +5,14 @@ export default function Links() {
     return (
         <div className="Links">
             <a href="https://www.linkedin.com/in/kathleen-stickel/" target="_blank" rel="noopener noreferrer">
-                    <img src="https://i.imgur.com/5YPzVDK.png" className="social-icon" id="li" alt="LinkedIn Logo"></img>
+                    <img src="https://i.imgur.com/XugcbSX.png" className="social-icon" id="li" alt="LinkedIn Logo"></img>
             </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <div className="triangles">
                 <div className="up"><p>About<br />Me</p></div>
                 <div className="down"><p>My Projects</p></div>
             </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <a href="https://github.com/kstick9210" target="_blank" rel="noopener noreferrer">
-                    <img src="https://i.imgur.com/3J01ITc.png" className="social-icon" alt="GitHub Logo"></img>
+                    <img src="https://i.imgur.com/71WSpc0.png" className="social-icon" alt="GitHub Logo"></img>
             </a>
         </div>
     )
