fe8151c86284f0dd34096ab1fe364ac662c29c24
Kathleen Stickel <kstick9210@outlook.com>
2020-12-21 20:42:44 -0500

    update

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -6,11 +6,13 @@ export default function Links() {
         <div className="Links">
             <a href="https://github.com/kstick9210" target="_blank" rel="noopener noreferrer">
                 <img src="https://i.imgur.com/71WSpc0.png" className="social-icon" alt="GitHub Logo"></img>
-            </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </a>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <div className="triangles">
                 <div className="up"><p>About<br />Me</p></div>
                 <div className="down"><p>My Projects</p></div>
-            </div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <a href="https://www.linkedin.com/in/kathleen-stickel/" target="_blank" rel="noopener noreferrer">
                 <img src="https://i.imgur.com/XugcbSX.png" className="social-icon" id="li" alt="LinkedIn Logo"></img>
             </a>
